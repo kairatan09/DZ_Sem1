85e7ff258f1cbdf406110e9341d8d5f70da891eb
Kairat Nagmetov <kairatan09@gmail.com>
2023-02-11 23:21:46 +0500

    Task8

---
+++ Task-8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите цельное цисло:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int index = 2; index <= number; index += 2)
+{
+  Console.Write(index + " ");
+}
