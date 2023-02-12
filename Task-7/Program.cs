d683abb147b630b3719c735c1f4756a6e6883528
Kairat Nagmetov <kairatan09@gmail.com>
2023-02-12 23:03:23 +0500

    Task7

---
+++ Task-7/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 7 HARD по желанию - идет за 2 необязательных Напишите программу, 
+// которая принимает на вход целое число любой разрядности 
+// и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+// 456111 -> 5
+// 78 -> 8
+// 9146548 -> 1
+// 3 -> нет
+
+Console.WriteLine("Введите любое целое цисло:");
+int number = Convert.ToInt32(Console.ReadLine());
+// int index = 1;
+int index = Convert.ToInt16(Math.Log10(number));
+
+if (number < 10)
+
+  Console.WriteLine("Нет");
+
+else if (number < 100)
+
+  Console.WriteLine(number % 10);
+
+else
+{
+  int temp1 = Convert.ToInt32(Math.Pow(10, index - 1));
+  int temp2 = number / temp1;
+  int temp3 = Convert.ToInt32(Math.Pow(10, index - 2));
+  int temp4 = number / temp3;
+  int temp5 = temp4 - temp2 * 10;
+  Console.WriteLine(temp5);
+}
